83d4a1ae5347f473eef675220fbc00cd38262f00
hoductrung1211 <hoductrung2604@gmail.com>
2023-06-28 09:33:00 +0700

    add notification

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,8 +1,19 @@
+'use client';
+import useNotification from "@/utils/hooks/useNotification"
 export default function Page() {
+    const notify = useNotification();
+
+    function handleClick() {
+        notify({
+            text: "hello",
+            type: "info", // 'info' | 'danger' | 'error' | 'success'
+        })
+    }
     return (
         <div>
             <h1 className="text-2xl font-bold">Home</h1>
             <p>Click to navigation on the left</p>
+            <button className="bg-green-300" onClick={handleClick}>Click me</button>
         </div>
     )
 }
--- app/home/products/page.jsx
+++ app/home/products/page.jsx
@@ -1,5 +1,19 @@
+'use client';
+import useNotification from "@/utils/hooks/useNotification"
 export default function Page() {
+    const notify = useNotification();
+
+    function handleClick() {
+        notify({
+            text: "hello",
+            type: "info", // 'info' | 'danger' | 'error' | 'success'
+        })
+    }
+
     return (
-        <h1>Products hello</h1>
+        <>
+            <h1>Products hello</h1>
+            <button onClick={handleClick}>Click me</button>
+        </>
     )
 }
